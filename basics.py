8fc8157eda1717be3d17be111696e32f141993aa
mickeymarse <mmserrapica@tutanota.com>
2024-03-15 10:53:55 +0000

    First lesson completed

---
+++ basics.py
@@ -0,0 +1,11 @@
+# print('line 1')
+# test = 123
+# print(test)
+# print(type(test))
+# test = 'ameba'
+# print(test)
+# print(type(test))
+
+user_name = input("What's your name? ")
+print(f"Hello, {user_name}. Nice to meet you.")
+print("Have a nice day,",user_name + '.')
