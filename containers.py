0bc93207c9b7184afaffb51d7e8ab0f6f86b5f64
mickeymarse <mmserrapica@tutanota.com>
2024-03-15 14:30:57 +0000

    Third lesson completed

---
+++ containers.py
@@ -0,0 +1,28 @@
+# a_tuple = (1,2,3,"string")
+# a_list = [1,2,3,"string", 3, 2]
+
+# print(len(a_list))
+# a_list.append("another word")
+# print(a_list)
+
+# a_set = {1,2,3,4,4}
+# print(a_set)
+
+# print(list(set(a_list)))
+
+#a_dictionary = {"key": "value", 123: [1,2,3]}
+# print(a_dictionary)
+
+# user_list = ["lisa", "bob", "alex", "anna", "john"]
+# print(user_list[0])
+# print(user_list[-1])
+# print(user_list[0:3:2])
+# a_dictionary["new_key"] = 123
+# print(a_dictionary["new_key"])
+
+# exercise
+ex_list = [1,2,3,4,5,6,7,8,9,10]
+# nu_list = ex_list[1:9:2]
+# nu_list.reverse()
+nu_list = ex_list[7::-2]
+print(nu_list)
