f3b737a323cb23d37b440fad6c3a265d594484fd
mickeymarse <mmserrapica@tutanota.com>
2024-03-15 15:15:16 +0000

    Fourth lesson completed

---
+++ flow.py
@@ -0,0 +1,46 @@
+# print(5 == 5)
+# if statements
+# if 5 > 4:
+#     print("correct")
+# elif 2 > 1:
+#     print("smth else")
+# elif 0 == 0 and 5 > 1:
+#     print("blabla")
+# else:
+#     print("incorrect")
+
+# while loop
+# counter = 0
+# while counter <= 10:
+#     print(counter)
+#     counter += 1
+
+# print("while loop has finished")
+
+# for loop
+#list = [1,2,3,4,5]
+# list = { 1:2, 3:4, 5:6}
+# # for x in list.items():
+# #    print(x)
+# for k,v in list.items():
+#     print(f"key: {k} and value: {v}")
+
+# Truthy and Falsy
+# if [1]:
+#     print("Truthy")
+# else:
+#     print("Falsy")
+
+# EXERCISE
+list = (1,2,3,4,5)
+
+for x in list:
+    if x == 2:
+        print("the value is 2")
+    elif x == 5:
+        counter = 0
+        while counter < 6:
+            print("last item")
+            counter += 1
+    else:
+        print("the value is not 2")
